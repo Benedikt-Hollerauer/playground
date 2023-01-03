ea164d5c86d354e80b3ff932020e791d13d72ec9
Benedikt Hollerauer <hollibene@gmail.com>
2023-01-03 11:44:19 +0100

    updated NotionApi

--- scala/src/main/scala/NotionApi.scala
+++ scala/src/main/scala/NotionApi.scala
@@ -1,14 +1,21 @@
 import notion.api.v1.*
+import notion.api.v1.model.databases.DatabaseProperty.RichText
+import notion.api.v1.model.pages.PageProperty
+import notion.api.v1.request.databases.UpdateDatabaseRequest
 
 import scala.util.{Failure, Success, Try}
 
 object NotionApi extends App:
 
-	val client = Try(
-		NotionClient("secret_QbXZdGvaEWNw7LJcMvavDk9acBqyXXEgiIltsuTsT1i")
-			.retrieveDatabase("0a1bfb4049dc4bc4b9d302ecbc47dd53")
+	val client = NotionClient("secret_QbXZdGvaEWNw7LJcMvavDk9acBqyXXEgiIltsuTsT1i")
+
+	val querySalesMonitoring = Try(
+		client.queryDatabase("0a1bfb4049dc4bc4b9d302ecbc47dd53", null, null, null, null)
 	)
 
-	client match
+	querySalesMonitoring match
 		case Failure(exception) => println(exception.toString)
-		case Success(value) => println(value)
+		case Success(value) =>
+			(1 to 10).foreach(println)
+			println(value)
+			(1 to 10).foreach(println)
