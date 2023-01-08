698332ee795888fc1e0c6c573f587456cc06dcd3
Benedikt Hollerauer <hollibene@gmail.com>
2023-01-08 20:04:36 +0100

    added createSale and queryDatabase functions

--- typeScript/notionApi/src/notionApi.ts
+++ typeScript/notionApi/src/notionApi.ts
@@ -1,9 +1,61 @@
 import { Client } from "@notionhq/client"
+import { CreatePageResponse, GetDatabaseResponse, QueryDatabaseResponse } from "@notionhq/client/build/src/api-endpoints"
 
 const notion = new Client({auth: process.env.NOTION_API_KEY})
 
-export async function retrieveDatabase(databaseId: string) {
+export class SaleIds {
+
+    databaseId: string
+    titleId: string
+
+    constructor(
+        databaseId: string,
+        titleId: string
+    ) {
+        this.databaseId = databaseId
+        this.titleId = titleId
+    }
+}
+
+export class Sale {
+
+    title: string
+    
+    constructor(
+        title: string
+    ) {
+        this.title = title
+    }
+}
+
+export async function retrieveDatabase(databaseId: string): Promise<GetDatabaseResponse> {
     return await notion.databases.retrieve({
         database_id: databaseId
+    })
+}
+
+export async function queryDatabase(databaseId: string): Promise<QueryDatabaseResponse> {
+    return await notion.databases.query({
+        database_id: databaseId
+    })
+}
+
+export async function createSale(saleIds: SaleIds, sale: Sale): Promise<CreatePageResponse> {
+    return await notion.pages.create({
+        parent: {
+            database_id: saleIds.databaseId
+        },
+        properties: {
+            [saleIds.titleId]: {
+                title: [
+                    {
+                        type: "text",
+                        text: {
+                            content: sale.title,
+                        }
+                    }
+                ]
+            }
+        }    
     })
 }
--- typeScript/notionApi/src/server.ts
+++ typeScript/notionApi/src/server.ts
@@ -1,6 +1,6 @@
 require("dotenv").config({ path: "./.env" })
 import express, { Request, Response } from "express"
-import { retrieveDatabase } from "./notionApi"
+import { createSale, queryDatabase, retrieveDatabase, Sale, SaleIds } from "./notionApi"
 
 const app = express()
 
@@ -8,10 +8,17 @@ app.get('/', (req: Request, res: Response) => {
     res.send("Hello World")
 })
 
+const saleIds: SaleIds = new SaleIds(
+    process.env.NOTION_DATABASE_ID as string,
+    process.env.TITLE_ID as string
+)
+
+const sale: Sale = new Sale(
+    "test"
+)
+
+createSale(saleIds, sale).then(x => console.log(x))
+
 console.log("go to: http://localhost:8080/")
 
 app.listen(process.env.PORT)
-
-const databaseId: string = process.env.NOTION_DATABASE_ID as string
-
-console.log(retrieveDatabase(databaseId))
